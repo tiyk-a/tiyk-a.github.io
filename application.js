c695b457e8d4dc2fd68b502ce0b4e371ef57e60a
Chiara <milejanaja@gmail.com>
2022-03-14 13:39:31 +0900

    🧚‍♀️ Updated!

--- application.js
+++ application.js
@@ -1,41 +1,18 @@
-// if(typeof jQuery != "undefined"){ //jQueryの読み込み確認
-//     $(function(){
-//         alert('jQuery is ready.')
-//     });
-// }
+// // https://ics.media/entry/14771/
+window.addEventListener("DOMContentLoaded", init);
 
-$(document).ready(function(){
-	$('#first').hide().fadeIn(1000);
-});
+function init() {
+  var elem = document.getElementById('target');
+  if (elem.innerHTML === '') {
+    elem.innerHTML = 'default';
+  } else {
+    console.log(elem);
+  }
+}
 
-// $(document).ready(function() {
-//   $('body').hide().fadeIn(2000);
-// });
-
-// CLICK AND JUMP
-$(function(){
-  $('a[href^="#"]').click(function(){
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-  });
-});
-// CLICK AND JUMP
-
-$(document).on("mouseover", "#ngm", function(){
-	$('#wyo-details, #lbot-details').css('display', 'none');
-	$('#ngm-details').css("display", "block");
-});
-
-$(document).on("mouseover", "#wyo", function(){
-	$('#ngm-details, #lbot-details').css('display', 'none');
-	$('#wyo-details').css("display", "block");
-});
-
-$(document).on("mouseover", "#lbot", function(){
-	$('#wyo-details, #ngm-details').css('display', 'none');
-	$('#lbot-details').css("display", "block");
-});
+function acceptTitle() {
+  const formElements = document.forms.info;
+  var arg1 = document.getElementById('target');
+  arg1.innerHTML = formElements.title.value;
+  init();
+}
